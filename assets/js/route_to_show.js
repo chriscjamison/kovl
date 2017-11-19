74ecc53b9352d0027be4457d63c7fbc725bbaaa3
Chris Jamison <chris@chriscjamison.com>
2017-11-18 22:24:55 -0800

    0.1 - 25% complete with index.htm  * HTML structure complete  * Currently styling header

--- assets/js/route_to_show.js
+++ assets/js/route_to_show.js
@@ -1,19 +1,21 @@
 function routeToShow()  {
-  var time_of_day = new Date;
+  var current_time = new Date;
+  var current_hour = new Number;
+  // var current_minute = new Number;
   var user_agent = window.navigator.userAgent;
 
-  time_of_day = time_of_day.getUTCHours();
+  current_hour = current_time.getUTCHours();
+  // current_minute = current_time.getMinutes();
 
-  if ((time_of_day >= -2) && 
-      (time_of_day < 0))  {
-
+  if (current_hour >= 22)  {
+    
     if (user_agent.indexOf("Mac OS X") > -1) {
       window.location.href="http://test.thewuinthelou.com/media-player/discussions.htm";
     } else {
       window.open('media-player/discussions.htm', 'media_player', 'width=640,height=480,location=no,menubar=no,resizeable=no,status=no,toolbar=no');
     }
-  } else if ((time_of_day >= 0) && 
-             (time_of_day < 3)) {
+  } else if ((current_hour >= 0) && 
+             (current_hour < 3)) {
 
     if (user_agent.indexOf("Mac OS X") > -1) {
       window.location.href="http://test.thewuinthelou.com/media-player/easy_treatment.htm";
