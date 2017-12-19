07cb6ac0f0a434e24b669d978b5b3a33f4be1d50
Chris Jamison <chris@chriscjamison.com>
2017-12-18 17:58:03 -0800

    0.2 - KOVL pre-launch

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,5 @@
 $(document).ready( 
   function () {
-    function routeToShow()  {
-      var current_time = new Date;
-      var current_hour = new Number;
-      // var current_minute = new Number;
-      var user_agent = window.navigator.userAgent;
-    
-      current_hour = current_time.getUTCHours();
-      // current_minute = current_time.getMinutes();
-    
-      if ((current_hour >= 0) && 
-          (current_hour < 3))  {
-        if (user_agent.indexOf("Mac OS X") > -1) {
-          window.location.href="http://kovlradio.com/media-player/easy_treatment.htm";
-        } else {
-          window.open('media-player/easy_treatment.htm', 'media_player', 'width=640,height=480,location=no,menubar=no,resizeable=no,status=no,toolbar=no');
-        }
-      } else {
-        if (user_agent.indexOf("Mac OS X") > -1) {
-          window.location.href="http://kovlradio.com/media-player/live_with_raej.htm";
-        } else {
-          window.open('media-player/discussions.htm', 'media_player', 'width=640,height=480,location=no,menubar=no,resizeable=no,status=no,toolbar=no');
-        }
-      }
-    }
-
-    window.alert("1");
-    
     $("nav > ul > li:nth-child(2) a").on("click", 
       function () {
 
@@ -55,3 +28,28 @@ $(document).ready(
     );
   }
 );
+
+function routeToShow()  {
+  var current_time = new Date;
+  var current_hour = new Number;
+  // var current_minute = new Number;
+  var user_agent = window.navigator.userAgent;
+
+  current_hour = current_time.getUTCHours();
+  // current_minute = current_time.getMinutes();
+
+  if ((current_hour >= 0) && 
+      (current_hour < 3))  {
+    if (user_agent.indexOf("Mac OS X") > -1) {
+      window.location.href="http://kovlradio.com/media-player/easy_treatment.htm";
+    } else {
+      window.open('media-player/easy_treatment.htm', 'media_player', 'width=640,height=480,location=no,menubar=no,resizeable=no,status=no,toolbar=no');
+    }
+  } else {
+    if (user_agent.indexOf("Mac OS X") > -1) {
+      window.location.href="http://kovlradio.com/media-player/live_with_raej.htm";
+    } else {
+      window.open('media-player/discussions.htm', 'media_player', 'width=640,height=480,location=no,menubar=no,resizeable=no,status=no,toolbar=no');
+    }
+  }
+}
