55173abf6e65120487d67a7e52497809a9c8bd1e
Chris Jamison <chris@chriscjamison.com>
2017-12-18 17:58:03 -0800

    0.2 - KOVL pre-launch

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready( 
   function () {
+<<<<<<< HEAD
     var header_selector = new String();
     var show_selector = new String();
     var footer_selector = new String();
@@ -143,6 +144,9 @@ $(document).ready(
     }
 
     $("#nav-link-shows").click(
+=======
+    $("nav > ul > li:nth-child(2) a").on("click", 
+>>>>>>> 0.2 - KOVL pre-launch
       function () {
 
         var shows_menu_selector = new String();
@@ -170,6 +174,7 @@ $(document).ready(
   }
 );
 
+<<<<<<< HEAD
 
 
 function routeToShow()  {
@@ -213,4 +218,29 @@ function determinePathofWebpage()  {
   }
 
   return path_string;
+=======
+function routeToShow()  {
+  var current_time = new Date;
+  var current_hour = new Number;
+  // var current_minute = new Number;
+  var user_agent = window.navigator.userAgent;
+
+  current_hour = current_time.getUTCHours();
+  // current_minute = current_time.getMinutes();
+
+  if ((current_hour >= 0) && 
+      (current_hour < 3))  {
+    if (user_agent.indexOf("Mac OS X") > -1) {
+      window.location.href="http://kovlradio.com/media-player/easy_treatment.htm";
+    } else {
+      window.open('media-player/easy_treatment.htm', 'media_player', 'width=640,height=480,location=no,menubar=no,resizeable=no,status=no,toolbar=no');
+    }
+  } else {
+    if (user_agent.indexOf("Mac OS X") > -1) {
+      window.location.href="http://kovlradio.com/media-player/live_with_raej.htm";
+    } else {
+      window.open('media-player/discussions.htm', 'media_player', 'width=640,height=480,location=no,menubar=no,resizeable=no,status=no,toolbar=no');
+    }
+  }
+>>>>>>> 0.2 - KOVL pre-launch
 }
